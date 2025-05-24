4925f001847bfb936b47d65df14eefa20a89eed1
Piyush-singh001 <piyushsengar243@gmail.com>
2025-05-24 21:53:00 +0530

    Updated sumOfArray program

--- src/day1.java
+++ src/day1.java
@@ -1,15 +1,40 @@
 //Print the array in reverse order.
 public class day1 {
     public static void main(String[] args) {
-        int []arr ={3,5,7,2,6};
-        for (int i=0;i< arr.length/2;i++){
-            int temp = arr[i];
-            arr[i]=arr[arr.length-1-i];
-            arr[arr.length-1-i]=temp;
-        }
+//        int []arr ={3,5,7,2,6};
+//        for (int i=0;i< arr.length/2;i++){
+//            int temp = arr[i];
+//            arr[i]=arr[arr.length-1-i];
+//            arr[arr.length-1-i]=temp;
+//        }
+//
+//        for (int i : arr){
+//            System.out.print(i+" ");}
 
-        for (int i : arr){
-            System.out.print(i+" ");
-        }
+
+        // Sum of all elements in an array
+
+//        int []arr = {3,5,7,2,7,8};
+//        int sum =0;
+//        for (int i =0; i< arr.length;i++){
+//            sum +=arr[i];
+//        }
+//        System.out.println("sum of array is "+sum);
+
+
+        // Find maximum element in an array
+
+//        int []arr={4,77,446,88,33,755};
+//        int max=0;
+//        for(int i=0;i< arr.length;i++){
+//            if(arr[i]>max){
+//                max=arr[i];
+//            }
+//        }
+//        System.out.println(max);
+
     }
 }
+
+
+
